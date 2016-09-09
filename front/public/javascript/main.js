6c9d83ba74dab01c219c1275c8039c7290359189
tupiznak <akej-vonavi@mail.ru>
2016-09-10 02:15:05 +0300

    add gun without relative of tank

--- front/public/javascript/main.js
+++ front/public/javascript/main.js
@@ -4,32 +4,53 @@ let RIGHT_KEY=39;
 let LEFT_KEY=37;
 let UP_KEY=38;
 let DOWN_KEY=40;
+let SPACE_KEY=32;
+let A_KEY=65;
+let S_KEY=83;
+let D_KEY=68;
+let W_KEY=87;
 
-function rotateTank(tank, diraction){
-    tank.classList.toggle("rotation_right",false);
-    tank.classList.toggle("rotation_up",false);
-    tank.classList.toggle("rotation_left",false);
-    tank.classList.toggle("rotation_down",false);
-    tank.classList.toggle(diraction,true);
+function rotateElem(elem, diraction){
+    elem.classList.toggle("rotation_right",false);
+    elem.classList.toggle("rotation_up",false);
+    elem.classList.toggle("rotation_left",false);
+    elem.classList.toggle("rotation_down",false);
+    elem.classList.toggle(diraction,true);
 }
 
 function handle(e) {
     if (e.keyCode === RIGHT_KEY) {
-        console.log('right');
+        //console.log('right');
         tank.style.left = isNaN(parseInt(tank.style.left)) ? 0 : parseInt(tank.style.left) + 10 + "px";
-        rotateTank(tank,"rotation_right");
+        gun.style.left = isNaN(parseInt(gun.style.left)) ? 0 : parseInt(gun.style.left) + 10 + "px";
+        rotateElem(tank,"rotation_right");
         //tank.classList.toggle("rotation_right",true);
     } else if (e.keyCode === UP_KEY) {
-        console.log('up');
+        //console.log('up');
         tank.style.top = isNaN(parseInt(tank.style.top)) ? 0 : parseInt(tank.style.top) - 10 + "px";
-        rotateTank(tank,"rotation_up");
+        gun.style.top = isNaN(parseInt(gun.style.top)) ? 0 : parseInt(gun.style.top) - 10 + "px";
+        rotateElem(tank,"rotation_up");
     } else if (e.keyCode === LEFT_KEY) {
-        console.log('left');
+        //console.log('left');
         tank.style.left = isNaN(parseInt(tank.style.left)) ? 0 : parseInt(tank.style.left) - 10 + "px";
-        rotateTank(tank,"rotation_left");
+        gun.style.left = isNaN(parseInt(gun.style.left)) ? 0 : parseInt(gun.style.left) - 10 + "px";
+        rotateElem(tank,"rotation_left");
     } else if (e.keyCode === DOWN_KEY) {
-        console.log('down');
+        //console.log('down');
         tank.style.top = isNaN(parseInt(tank.style.top)) ? 0 : parseInt(tank.style.top) + 10 + "px";
-        rotateTank(tank,"rotation_down");
+        gun.style.top = isNaN(parseInt(gun.style.top)) ? 0 : parseInt(gun.style.top) + 10 + "px";
+        rotateElem(tank,"rotation_down");
+    } if (e.keyCode === D_KEY) {
+        rotateElem(gun,"rotation_right");
+    } else if (e.keyCode === W_KEY) {
+        rotateElem(gun,"rotation_up");
+    } else if (e.keyCode === A_KEY) {
+        rotateElem(gun,"rotation_left");
+    } else if (e.keyCode === S_KEY) {
+        rotateElem(gun,"rotation_down");
+    } if (e.keyCode === SPACE_KEY) {
+        //console.log('down');
+        tank.style.top = isNaN(parseInt(tank.style.top)) ? 0 : parseInt(tank.style.top) + 10 + "px";
+        rotateElem(tank,"rotation_down");
     }
 }
